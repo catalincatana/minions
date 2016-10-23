2d07d7fc709657fa14bd321ce773ce2ecf15d02d
Catalin Catana <catalin.catana@endava.com>
2016-10-23 22:09:32 +0300

    My First Commit

---
+++ src/main/java/com/endava/main/Main.java
@@ -0,0 +1,14 @@
+package com.endava.main;
+
+import com.endava.users.CatalinC;
+
+/**
+ * Created by ccatana on 10/23/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("********************");
+        System.out.println("And the winners are:");
+        CatalinC.myName();
+    }
+}
---
+++ src/main/java/com/endava/users/CatalinC.java
@@ -0,0 +1,10 @@
+package com.endava.users;
+
+/**
+ * Created by ccatana on 10/23/2016.
+ */
+public class CatalinC {
+    public static void myName(){
+        System.out.println("Catalin Catana");
+    }
+}
