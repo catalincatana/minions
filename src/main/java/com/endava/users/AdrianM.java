11eeb4cd28caed4052585528d0d9dda19b71f7ef
Adrian Marcu <admarcu@endava.com>
2016-10-24 15:29:18 +0300

    Adrian

---
+++ src/main/java/com/endava/users/AdrianM.java
@@ -0,0 +1,10 @@
+package com.endava.users;
+
+/**
+ * Created by admarcu on 10/24/2016.
+ */
+public class AdrianM {
+    public static void myName(){
+        System.out.println("Marcu Constantin-Adrian");
+    }
+}
