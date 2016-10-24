fe004fd284db05b8af2d6e07404a7965ead6144d
Iulia Nastase <inastase@endava.com>
2016-10-24 16:34:47 +0300

    commit tema!

---
+++ src/main/java/com/endava/users/IuliaN.java
@@ -0,0 +1,10 @@
+package com.endava.users;
+
+/**
+ * Created by inastase on 10/24/2016.
+ */
+public class IuliaN {
+    public static void myName(){
+        System.out.println("Iulia Nastase");
+    }
+}
