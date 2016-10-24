a50984b4bc39e9030263541fd414a17aadcf0760
Gianina Atomulesei <geanina.at@gmail.com>
2016-10-24 17:37:42 +0300

    msg

---
+++ src/main/java/com/endava/users/GeaninaA.java
@@ -0,0 +1,10 @@
+package com.endava.users;
+
+/**
+ * Created by gatomulesei on 10/24/2016.
+ */
+public class GeaninaA {
+    public static void myName(){
+        System.out.println("Geanina Atomulesei");
+    }
+}
